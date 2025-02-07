4e3588cc109cd83f5858a05be84e87e96f3fd7bc
CinthiaDayanara <cdchuraco@uce.edu.ec>
2025-02-07 08:49:18 -0500

    ypate

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use('/api', listRoutes);
 
 // Puerto y servidor
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
