6f987c7516f071847fcbd75e3dbb59b22475be3b
CinthiaDayanara <cdchuraco@uce.edu.ec>
2025-02-08 16:54:20 -0500

    PRUEBA12

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 
 // Configuración de conexión a PostgreSQL
 const client = new Client({
-  host: process.env.DB_SERVER,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
