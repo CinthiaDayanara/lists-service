6e9d0a4a168b8c87c46088e8c15f56854e88ebcb
CinthiaDayanara <cdchuraco@uce.edu.ec>
2025-02-09 09:11:33 -0500

    cambios

--- src/app.js
+++ src/app.js
@@ -10,7 +10,11 @@ app.use(express.json());  // ✅ Middleware para procesar JSON
 
 app.use('/api', listRoutes);
 
+
+
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
 });
+
+
